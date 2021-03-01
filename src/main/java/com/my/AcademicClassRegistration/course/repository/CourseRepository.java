6c7f8c04ab0e48e7ab1489ecc391a2f5e1d1811a
harpreet <harpreet.singhprudential.com>
2021-03-01 09:58:38 +0000

    removed unwanted code

--- src/main/java/com/my/AcademicClassRegistration/course/repository/CourseRepository.java
+++ src/main/java/com/my/AcademicClassRegistration/course/repository/CourseRepository.java
@@ -8,45 +8,4 @@ import com.my.AcademicClassRegistration.course.model.Course;
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Integer>  {
 
-	/*private static List<Course> courseList = new ArrayList<Course>();
-	static int counter = 2;
-	static Integer[] i = {1,2};
-	static {
-		courseList.add(new Course(1, "Course 1", i));
-		courseList.add(new Course(2, "Course 2", i));
-	}
-	
-	public int[] addCourses(Course[] courses)	{
-		
-		int[] courseIds = new int[courses.length];
-		int i=0;
-		for ( Course course : courses)	{
-			if( course.getCourseId() == null)	{
-				course.setCourseId(++counter);
-				courseList.add(course);
-			}
-			courseIds[i++] = course.getCourseId();
-		}
-		
-		return courseIds;
-	}
-	
-	
-	public List<Course> getCourses() {
-		return courseList;
-	}
-	
-	public Course findCourse(Integer courseId) {
-		
-		for( Course course : courseList) {
-			if( course.getCourseId().equals(courseId)) {
-				return course;
-			}
-		}
-		
-		return null;
-
-	}*/
-	
-
 }
